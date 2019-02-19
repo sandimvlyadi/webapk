50940e4b780d62bc146227ef61f415e74736f961
sandimvlyadi <sandimvlyadi@gmail.com>
2019-02-19 14:33:22 +0700

    loading

--- pages/login/login.js
+++ pages/login/login.js
@@ -4,12 +4,12 @@ function ping()
 {
 	$.get(baseurl + 'ajax_service?request=ping', function(response){
 		if ($('#onlineStatusMsg').length > 0) {
-			$('#onlineStatusMsg').slideUp('slow');
+			$('#onlineStatusMsg').slideUp('fast');
 			$('#onlineStatusMsg').remove();
 		}
 		setTimeout(function(){
 		    ping();
-		}, 60000);
+		}, 3000);
 	}, 'json')
 	.fail(function(jqXHR, textStatus, errorThrown){
 		if ($('#onlineStatusMsg').length == 0) {
@@ -17,10 +17,70 @@ function ping()
 		}
 		setTimeout(function(){
 			ping();
-		}, 5000);
+		}, 3000);
 	});
 }
 
+$('#login-form button').click(function(){
+	var email = $('input[type="email"]').val();
+	if (email == '') {
+		$('input[type="email"]').focus();
+		var div = $('#login-form div:eq(0)');
+		div.addClass('has-error');
+		var errMsg = $('<span class="help-block"></span>');
+		errMsg.append('Email can not be empty');
+		$('#login-form div:eq(0)').append(errMsg.slideDown('slow').delay(3000).slideUp('fast'));
+		setTimeout(function(){
+			errMsg.remove();
+		}, 4000);
+		return;
+	} else{
+		if (email.indexOf('@') > 0 && email.indexOf('.') > 0) {
+			var div = $('#login-form div:eq(0)');
+			div.removeClass('has-error');
+		} else{
+			$('input[type="email"]').focus();
+			var div = $('#login-form div:eq(0)');
+			div.addClass('has-error');
+			var errMsg = $('<span class="help-block"></span>');
+			errMsg.append('Please enter your e-mail address correctly');
+			$('#login-form div:eq(0)').append(errMsg.slideDown('slow').delay(3000).slideUp('fast'));
+			setTimeout(function(){
+				errMsg.remove();
+			}, 4000);
+			return;
+		}
+	}
+
+	var paswd = $('input[type="password"]').val();
+	if (paswd == '') {
+		$('input[type="password"]').focus();
+		var div = $('#login-form div:eq(1)');
+		div.addClass('has-error');
+		var errMsg = $('<span class="help-block"></span>');
+		errMsg.append('Passowrd can not be empty');
+		$('#login-form div:eq(1)').append(errMsg.slideDown('slow').delay(3000).slideUp('fast'));
+		setTimeout(function(){
+			errMsg.remove();
+		}, 4000);
+		return;
+	} else{
+		var div = $('#login-form div:eq(1)');
+		div.removeClass('has-error');
+	}
+
+	$(this).attr('disabled', 'disabled');
+	$('.login-box').hide();
+	$('body').prepend('<div class="loading"><img class="imageRotateHorizontal" src="../../favicon.ico" /><p>Please Wait</p></div>');
+	setTimeout(function(){
+		$('.loading').remove();
+		$('.login-box').show();
+	}, 3000);
+
+});
+
 $(document).ready(function(){
 	ping();
+
+	$('#login-form button').removeAttr('disabled');
 });
