d45b7d2700cb861924b97fdbdf19062c2b54a263
sandimvlyadi <sandimvlyadi@gmail.com>
2019-02-25 19:14:36 +0700

    profile done

--- dist/js/main.js
+++ dist/js/main.js
@@ -100,11 +100,22 @@ function validateToken(dataToken)
 	}
 }
 
+function setFooter()
+{
+	var date = new Date();
+	var year = date.getFullYear();
+	if (year == '2019') {
+		$('.main-footer').html('<strong>Copyright &copy; 2019.</strong> All rights reserved.');
+	} else{
+		$('.main-footer').html('<strong>Copyright &copy; 2019 - '+ year +'.</strong> All rights reserved.');
+	}
+}
+
 $('#btnProfile').click(function(){
-	
+	window.location.replace('../../pages/profile/profile.html');
 });
 
-$('#btnSignOut').click(function(){
+$('#btnSignOut, a[href="#signout"]').click(function(){
 	window.localStorage.clear();
 	window.location.replace('../../pages/login/login.html');
 });
@@ -115,6 +126,21 @@ $('body').prepend(pleasewait);
 $(document).ready(function(){
 	ping();
 	validateToken(tokenData);
+	setFooter();
+
+	if (userData != null) {
+		if (userData.display_picture != '') {
+			$('.user-header img').attr('src', userData.display_picture);
+			$('.user-menu a img').attr('src', userData.display_picture);
+			$('.user-panel .image img').attr('src', userData.display_picture);
+		}
+
+		$('.user-header p').replaceWith('<p>' + userData.fullname + '<small>' + userData.company_name + '</small></p>');
+		$('.user-panel .info').remove();
+		$('.user-panel').append('<div class="full-left info">');
+		$('.user-panel .info').append('<p>'+ userData.fullname +'</p>');
+		$('.user-panel .info').append('<i class="fa fa-home text-success"></i> ' + userData.company_name.substring(0, 17));
+	}
 	
 	if (sessionStorage.getItem('msgResult') !== null) {
 		if (sessionStorage.getItem('msgResult') === 'true') {
